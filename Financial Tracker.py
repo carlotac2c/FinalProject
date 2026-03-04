9b3e1f94b302b2684ac5e3d10f8f26732e80f821
carlotac2c <carlotac2c@gmail.com>
2026-03-04 20:37:18 +0100

    Update Financial Tracker.py

--- Financial Tracker.py
+++ Financial Tracker.py
@@ -1,6 +1,6 @@
 #Our idea for this final project was to create a useful project that can be used by everyone.
 #Because of this, we decided to do a financial tracker, which allows you to input daily spending and
-#see where you're at and the spending you've done this month.
+#see where you're at and the spending you've done this month. carlota
 
 """
 Personal Finance Tracker (CSV-based) — User-friendly Quick Add
